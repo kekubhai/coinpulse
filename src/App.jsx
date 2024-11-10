ef632cd7ddc2033b958a602be82e232b1349eb95
Anirban Ghosh <anirbanghosh060@gmail.com>
2024-11-10 13:03:55 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
